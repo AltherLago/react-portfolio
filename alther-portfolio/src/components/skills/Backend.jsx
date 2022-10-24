65d0c026e2aa1f39f364d0a1bf28fbd24018c856
AltherLago <ajsl@ic.ufal.br>
2022-10-24 12:19:25 -0300

    Finish skills

--- alther-portfolio/src/components/skills/Backend.jsx
+++ alther-portfolio/src/components/skills/Backend.jsx
@@ -4,7 +4,7 @@ export default function Backend(){
     return (
 
         <div className="skills__content">
-            <h3 className="skills__title"></h3>
+            <h3 className="skills__title">Backend Developer</h3>
 
             <div className="skills__box">
                 <div className="skills__group">
@@ -12,24 +12,24 @@ export default function Backend(){
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
+                            <h3 className="skills__name">Node Js</h3>
+                            <span className="skills__level">Quero aprender</span>
                         </div>
                     </div>
 
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
+                            <h3 className="skills__name">Python</h3>
+                            <span className="skills__level">Basic</span>
                         </div>
                     </div>
 
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
+                            <h3 className="skills__name">MySQL</h3>
+                            <span className="skills__level">Básico</span>
                         </div>
                     </div>
 
@@ -39,7 +39,15 @@ export default function Backend(){
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name"></h3>
+                            <h3 className="skills__name">Java</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    {/*
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">Em construção</h3>
                             <span className="skills__level"></span>
                         </div>
                     </div>
@@ -47,18 +55,11 @@ export default function Backend(){
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name"></h3>
+                            <h3 className="skills__name">Em construção</h3>
                             <span className="skills__level"></span>
                         </div>
                     </div>
-
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-                        <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
-                        </div>
-                    </div>
+                    */}
 
                 </div>
          
