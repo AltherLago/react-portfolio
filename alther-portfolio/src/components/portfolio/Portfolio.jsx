157345adaa21c555378bd47a1f6e9d5b7de32ac0
AltherLago <ajsl@ic.ufal.br>
2022-10-25 15:14:24 -0300

    almost finish portfolio

--- alther-portfolio/src/components/portfolio/Portfolio.jsx
+++ alther-portfolio/src/components/portfolio/Portfolio.jsx
@@ -2,24 +2,51 @@ import React from 'react'
 import "./portfolio.css"
 import { Data } from './Data'
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
 const Portfolio = () => {
     return (
-        <section className="portifolio container section">
+        <section className="portfolio container section">
             <h2 className="section__title">Meu Portifólio</h2>
             <span className="section__subtitle">Projetos Recentes</span>
             
-            <div className="portfolio__container">
+            <Swiper className="portfolio__container"
+                loop={true}
+                grabCursor={true}
+                spaceBetween={24}
+                pagination={{
+                  clickable: true,
+                }}
+                breakpoints={{
+                  576: {
+                    slidesPerView: 2,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 48,
+                  },
+                }}
+                modules={[Pagination]}
+            >
                 {Data.map(({id, image, title}) => {
                     return (
-                        <div className="portfolio__card" key={id}>
+                        <SwiperSlide className="portfolio__card" key={id}>
                             <img src={image} alt="i"
                             className='portfolio__img'/>
-
+                        {/*TODO link for project*/}
                             <h3 class="portfolio__name">{title}</h3>
-                        </div>
+                        </SwiperSlide>
                     )
                 })}
-            </div>
+            </Swiper>
         </section>
     )
 }
